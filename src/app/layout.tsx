0cf0b2649120ca5d2a5984c592955aaa1e5bcf0c
yspakk <gvbn99@naver.com>
2026-03-12 16:18:52 +0900

    fix: 빌드오류 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,15 @@
 import { Geist } from "next/font/google";
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ko">
+      <body className={geist.variable}>{children}</body>
+    </html>
+  );
+}
