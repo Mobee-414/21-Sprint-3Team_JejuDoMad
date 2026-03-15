78f9edc8a1dee3755cf98ae67057b648c6e2164f
Mobee-414 <albertpaj0214@gmail.com>
2026-03-15 18:43:07 +0900

    feat: initial commit

---
+++ src/components/gnb/gnb.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function GNB() {
+  const isLogin:boolean = false;
+
+  return (
+    <header className="w-full">
+      <div className="
+        mx-auto flex items-center justify-between px-[200px] md:h-[80px] h-[48px]
+      "
+      >
+        <Link href= "/" >
+          <Image 
+            src="images/icons/logo.svg"
+            alt="Global Nomad Logo"
+            width={174}
+            height={28}
+          />
+        </Link>
+
+        <div className="flex items-center gap-4">
+          {isLogin ? (
+            <>
+              <button>
+                <Image 
+                  src="images/icons/bell.svg"
+                  alt="Bell"
+                  width={24}
+                  height={24}
+                />
+              </button>
+              <span>정만철</span>
+            </>
+          ) : (
+            <>
+              <Link href="/login">로그인</Link>
+              <Link href="/signup">회원가입</Link>
+            </>
+          )}
+        </div>
+
+      </div>
+    </header>
+  );   
+}
