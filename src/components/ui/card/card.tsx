b48488008dd57d0e7d10f6aff385ac9d5b452958
yspakk <gvbn99@naver.com>
2026-03-17 01:32:14 +0900

    feat:공통컴포넌트 Card

---
+++ src/components/ui/card/card.tsx
@@ -0,0 +1,56 @@
+import { CardProps } from "./types";
+import Image from "next/image";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+
+export default function Card({
+  bannerImageUrl,
+  title,
+  rating,
+  reviewCount,
+  price,
+  className,
+}: CardProps) {
+  return (
+    <Link
+      href={``}
+      className={cn(
+        "block w-[155px] max-w-[260px] overflow-hidden rounded-[18px] md:w-[260px] md:rounded-4xl",
+        className,
+      )}
+    >
+      <div className="relative aspect-[155/175] w-full md:aspect-[260/290]">
+        <Image
+          src={bannerImageUrl}
+          fill
+          alt={`${title}의 배너이미지`}
+          className="object-cover"
+        />
+      </div>
+      <div className="relative -mt-[34px] rounded-[18px] bg-white px-[17px] py-4 shadow-[0px_-4.5px_11.25px_rgba(0,0,0,0.05)] md:rounded-4xl md:px-[30px] md:py-5">
+        <h4 className="text-14 md:text-18 overflow-hidden font-bold text-ellipsis whitespace-nowrap md:leading-[26px]">
+          {title}
+        </h4>
+        <div className="text-12 md:text-14 mt-1 flex items-center font-medium md:mt-[2px]">
+          <Image
+            src="/images/icons/star.svg"
+            width={20}
+            height={20}
+            alt="별 아이콘"
+            className="mr-[3px] h-4 w-4 md:mr-[5px] md:h-5 md:w-5"
+          />
+          {rating}
+          <span className="text-gray-400 md:ml-[2px]">({reviewCount})</span>
+        </div>
+        <div className="mt-[10px] flex flex-wrap items-center gap-[2px] md:mt-[18px]">
+          <strong className="md:text-18 text-[15px] font-bold">
+            &#8361; {(price ?? 0).toLocaleString()}
+          </strong>
+          <span className="text-12 md:text-16 font-medium text-gray-400">
+            / 인
+          </span>
+        </div>
+      </div>
+    </Link>
+  );
+}
