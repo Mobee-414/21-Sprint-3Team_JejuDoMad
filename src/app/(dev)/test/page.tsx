4428c50308ee42fd3a8e233dbcc923c150edc292
jihyun <yomkkankka@gmail.com>
2026-03-16 13:22:25 +0900

    search-bar 파일 이동

--- src/app/(dev)/test/page.tsx
+++ src/app/(dev)/test/page.tsx
@@ -1,9 +1,11 @@
-import SearchBar from "@/features/myExperiences/search-bar";
+import SearchBar from "@/components/search-bar/SearchBar";
+import { Input } from "@/components/ui/input";
 
 export default function HomePage() {
   return (
-    <main className="flex justify-center py-20">
+    <main className="flex flex-col items-center justify-center py-20 gap-6">
       <SearchBar />
+      <Input className="w-60" placeholder="검색어를 입력하세요" />
     </main>
   );
 }
