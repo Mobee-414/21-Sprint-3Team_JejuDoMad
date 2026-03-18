27d72d8660881f18b7420a457bb939083ae15412
Mobee-414 <albertpaj0214@gmail.com>
2026-03-19 03:54:41 +0900

    [MT21-21] feat: 메인페이지 구성

---
+++ src/features/activities/components/AllActivitiesSection.tsx
@@ -0,0 +1,79 @@
+import Card from '@/components/ui/card/card';
+import CategoryFilter from './CategoryFilter';
+import PriceSortDropdown from "./PriceSortDropdown";
+import Pagination from "./Pagination"
+
+export default function AllActivitiesSection () {
+  return (
+    <div className="max-w-[1120px] mx-auto mt-20">
+      <h2 className="text-xl font-bold"> 모든 체험</h2>
+
+      <div className='flex justify-between items-center mt-6'>
+        <CategoryFilter />
+        <PriceSortDropdown />
+      </div>
+
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mt-6">
+          <Card 
+            bannerImageUrl="/images/photos/street_dance.svg"
+            title="함께 배우면 즐거운 스트릿 댄스"
+            rating={4.9}
+            reviewCount={703}
+            price={38000}
+          />
+          <Card 
+            bannerImageUrl="/images/photos/stepping_stones.svg"
+            title="연인과 사랑의 징검다리"
+            rating={3.9}
+            reviewCount={108}
+            price={35000}
+          />
+          <Card 
+            bannerImageUrl="/images/photos/vr.svg"
+            title="VR 게임 마스터 하는 법"
+            rating={4.9}
+            reviewCount={293}
+            price={38000}
+          />
+          <Card 
+            bannerImageUrl="/images/photos/camp.svg"
+            title="자연 속에서 캠핑하기"
+            rating={4.7}
+            reviewCount={236}
+            price={45000}
+          />
+          <Card 
+            bannerImageUrl="/images/photos/street_dance.svg"
+            title="함께 배우면 즐거운 스트릿 댄스"
+            rating={4.9}
+            reviewCount={703}
+            price={38000}
+          />
+          <Card 
+            bannerImageUrl="/images/photos/stepping_stones.svg"
+            title="연인과 사랑의 징검다리"
+            rating={3.9}
+            reviewCount={108}
+            price={35000}
+          />
+          <Card 
+            bannerImageUrl="/images/photos/vr.svg"
+            title="VR 게임 마스터 하는 법"
+            rating={4.9}
+            reviewCount={293}
+            price={38000}
+          />
+          <Card 
+            bannerImageUrl="/images/photos/camp.svg"
+            title="자연 속에서 캠핑하기"
+            rating={4.7}
+            reviewCount={236}
+            price={45000}
+          />
+        </div>
+        <div className='mt-10 mb-50'>
+          <Pagination />
+        </div>
+    </div>
+  )
+}
